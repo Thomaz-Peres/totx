f054b3f87177d587aadd4e1a57a06fc377ef257d
Thomaz Peres <thomaz.peres21@hotmail.com>
2024-08-20 21:06:08 -0300

    why

--- target/debug/deps/lexycal_analyser-059cc5baa21d0869.d
+++ target/debug/deps/lexycal_analyser-059cc5baa21d0869.d
@@ -1,10 +1,8 @@
-/home/tomenos/dev/Study-Notes/Computer_Science/Compiler_Interpreters/rust/lexycal_analyser/target/debug/deps/lexycal_analyser-059cc5baa21d0869: src/main.rs src/parser.rs src/parser/parser.rs src/lexycal.rs src/lexycal/scanner.rs src/lexycal/token.rs
+/home/tomenos/dev/totx/target/debug/deps/lexycal_analyser-059cc5baa21d0869: src/main.rs src/parser.rs src/scanner.rs src/token.rs
 
-/home/tomenos/dev/Study-Notes/Computer_Science/Compiler_Interpreters/rust/lexycal_analyser/target/debug/deps/lexycal_analyser-059cc5baa21d0869.d: src/main.rs src/parser.rs src/parser/parser.rs src/lexycal.rs src/lexycal/scanner.rs src/lexycal/token.rs
+/home/tomenos/dev/totx/target/debug/deps/lexycal_analyser-059cc5baa21d0869.d: src/main.rs src/parser.rs src/scanner.rs src/token.rs
 
 src/main.rs:
 src/parser.rs:
-src/parser/parser.rs:
-src/lexycal.rs:
-src/lexycal/scanner.rs:
-src/lexycal/token.rs:
+src/scanner.rs:
+src/token.rs:
